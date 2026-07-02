8262bc9c16681b242d1c5247400ec082dc503b88
yash-vishwari <yashgupta9329@gmail.com>
2026-07-02 22:35:43 +0530

    Updated App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,16 @@
 
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import SignUp from './Pages/SignUp'
-import SignIn from './Pages/SignIn'
+import Register from "./Pages/Register.jsx"
+import SignIn from './Pages/Login.jsx'
 
 
 function App() {
   return (
   <Routes>
-    <Route path ='/signup' element={<SignUp/>}/>
-    <Route path ='/signin' element={<SignIn/>}/>
+    
+    <Route path ='/register' element={<Register/>}/>
+    <Route path ='/login' element={<Login/>}/>
   </Routes>
 
   )
