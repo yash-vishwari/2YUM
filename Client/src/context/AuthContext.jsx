9876e7db092d3583926e233e6100e603a54c39aa
yash-vishwari <yashgupta9329@gmail.com>
2026-07-24 22:57:48 +0530

    Auth Context File

--- Client/src/context/AuthContext.jsx
+++ Client/src/context/AuthContext.jsx
@@ -3,17 +3,22 @@ import { useEffect, useState, createContext, useContext } from "react";
 const AuthContext = createContext();
 
 export const AuthProvider = (props) => {
-  const [userData, setUserData] = useState( JSON.parse(sessionStorage.getItem("UserData"))|| null);
+  const [user, setUserData] = useState(
+    JSON.parse(sessionStorage.getItem("UserData")) || null,
+  );
   const [isLogin, setIsLogin] = useState(!!userData);
-
+  const [role, setRole] = useState(userData ? userData.userType : null);
   useEffect(() => {
     setIsLogin(!!userData);
+    setRole(user ? user.userType : null);
   }, [userData]);
 
-  const data = { userData, setUserData, isLogin, setIsLogin };
+  const data = { userData, setUserData, isLogin, setIsLogin, role, setRole };
   return (
     <AuthContext.Provider value={data}>{props.children}</AuthContext.Provider>
   );
 };
+
+
 
 export const useAuth = () => useContext(AuthContext);
